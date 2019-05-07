ff797f18fea48c7bd792311ea3cd267ca8a12ce6
John <zhentaodai@foxmail.com>
2019-05-07 23:23:39 +0800

    Signed-off-by: John <zhentaodai@foxmail.com>

--- reinforcement_learning/Off-Policy MC Control with Weighted Importance Sampling Solution.py
+++ reinforcement_learning/Off-Policy MC Control with Weighted Importance Sampling Solution.py
@@ -24,7 +24,7 @@ def create_random_policy(nA):
 		A function that takes an observation as input and returns a vector
 		of action probabilities
 	"""
-	A = np.ones(nA, dtype=float) / nA
+	A = np.ones(nA, dtype=float)/nA
 	def policy_fn(observation):
 		return A
 	return policy_fn
@@ -70,61 +70,47 @@ def mc_control_importance_sampling(env, num_episodes, behavior_policy, discount_
 	# The final action-value function.
 	# A dictionary that maps state -> action values
 	Q = defaultdict(lambda: np.zeros(env.action_space.n))
-	# The cumulative denominator of the weighted importance sampling formula
-	# (across all episodes)
-	C = defaultdict(lambda: np.zeros(env.action_space.n))
 	
 	# Our greedily policy we want to learn
 	target_policy = create_greedy_policy(Q)
-		
-	for i_episode in range(1, num_episodes + 1):
-		# Print out which episode we're on, useful for debugging.
-		if i_episode % 1000 == 0:
-			print("\rEpisode {}/{}.".format(i_episode, num_episodes), end="")
+	
+	# Another initialization C that maps (s,a) pair to cumulative factor
+	C = defaultdict(float)
+	
+	# for each episode
+	for i_episode in range(1, num_episodes+1):
+		if i_episode%1000 == 0:
+			print("\rEpisode {}/{}.".format(i_episode, num_episodes),end="")
 			sys.stdout.flush()
-
-		# Generate an episode.
-		# An episode is an array of (state, action, reward) tuples
+			
+		# Generate an episode
 		episode = []
 		state = env.reset()
 		for t in range(100):
-			# Sample an action from our policy
-			probs = behavior_policy(state)
-			action = np.random.choice(np.arange(len(probs)), p=probs)
+			action = np.random.choice(range(env.action_space.n), p=behavior_policy(state))
 			next_state, reward, done, _ = env.step(action)
 			episode.append((state, action, reward))
 			if done:
 				break
 			state = next_state
-		
-		# Sum of discounted returns
-		G = 0.0
-		# The importance sampling ratio (the weights of the returns)
-		W = 1.0
-		# For each step in the episode, backwards
-		for t in range(len(episode))[::-1]:
-			state, action, reward = episode[t]
-			# Update the total reward since step t
-			G = discount_factor * G + reward
-			# Update weighted importance sampling formula denominator
-			C[state][action] += W
-			# Update the action-value function using the incremental update formula (5.7)
-			# This also improves our target policy which holds a reference to Q
-			Q[state][action] += (W / C[state][action]) * (G - Q[state][action])
-			# If the action taken by the behavior policy is not the action 
-			# taken by the target policy the probability will be 0 and we can break
-			if action !=  np.argmax(target_policy(state)):
+			
+		# for every (s,a) pair visited, perform weigted importance sampling
+		G, W = 0.0, 1.0
+		for s_a_pair in episode[::-1]:
+			state, action, reward = s_a_pair
+			G = discount_factor*G + reward
+			C[(state, action)] += W
+			Q[state][action] += W/C[(state, action)]*(G - Q[state][action])
+			if np.argmax(target_policy(state)) != action:
 				break
-			W = W * 1./behavior_policy(state)[action]
-		
+			W *= 1/behavior_policy(state)[action]
+			
 	return Q, target_policy
 	
-
 random_policy = create_random_policy(env.action_space.n)
 Q, policy = mc_control_importance_sampling(env, num_episodes=500000, behavior_policy=random_policy)
 
-
-with open('result1.txt', 'w') as fp:
+with open('result2.txt', 'w') as fp:
 	for k,v in Q.items():
 		a,b,c = k
 		d,e = v
@@ -132,8 +118,6 @@ with open('result1.txt', 'w') as fp:
 		seperator = ','
 		fp.write(seperator.join(line))
 		fp.write('\n')
-		
-		
 
 # For plotting: Create value function from action-value function
 # by picking the best action at each state
@@ -142,3 +126,4 @@ for state, action_values in Q.items():
 	action_value = np.max(action_values)
 	V[state] = action_value
 plotting.plot_value_function(V, title="Optimal Value Function")
+
